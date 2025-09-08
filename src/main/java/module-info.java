d26e7493beaeab2e44cfb88950668e3a37eea58d
vihan <vihanvimen46@gmail.com>
2025-09-08 22:41:31 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.its1155ormfinal_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.its1155ormfinal_project to javafx.fxml;
+    exports com.example.its1155ormfinal_project;
+}
